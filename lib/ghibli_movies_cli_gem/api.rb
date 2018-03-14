e4759cdfe6c93b192752c1b8e3db74ea7ab83e3a
ludaires <ludaires@gmail.com>
2018-03-13 22:48:00 -0400

    add method get_films

---
+++ lib/ghibli_movies_cli_gem/api.rb
@@ -0,0 +1,19 @@
+class GhibliMoviesCliGem::API
+
+    def self.get_films
+        response = RestClient.get('https://ghibliapi.herokuapp.com/films')
+        raw = JSON.parse(response)        
+        raw.map do |r|           
+            film = GhibliFilms::Film.new
+            film.id = r["id"]
+            film.title = r["title"]
+            film.description = r["description"]
+            film.director = r["director"]
+            film.producer = r["producer"]        
+            film.release_date = r["release_date"]        
+            film.score = r["rt_score"]    
+            film
+        end
+    end
+
+end
