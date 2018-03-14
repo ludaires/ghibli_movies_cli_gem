18794b883d06dc715f497957a301d6b1719bed20
ludaires <ludaires@gmail.com>
2018-03-13 22:49:26 -0400

    add require_relative api

--- lib/ghibli_movies_cli_gem.rb
+++ lib/ghibli_movies_cli_gem.rb
@@ -1,5 +1,6 @@
 require 'ghibli_movies_cli_gem/version'
 require_relative '../lib/ghibli_movies_cli_gem/cli'
+require_relative '../lib/ghibli_movies_cli_gem/api'
 
 module GhibliMoviesCliGem
   # Your code goes here...
