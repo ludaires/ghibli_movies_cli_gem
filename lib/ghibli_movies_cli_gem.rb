7cde6b8a9095e44d26605455c09816ccf97981d0
ludaires <ludaires@gmail.com>
2018-03-15 23:47:24 -0400

    include require rest-client, json, pry, pry-byebug

--- lib/ghibli_movies_cli_gem.rb
+++ lib/ghibli_movies_cli_gem.rb
@@ -1,6 +1,15 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+require 'pry-byebug'
+
+
+
+
 require 'ghibli_movies_cli_gem/version'
 require_relative '../lib/ghibli_movies_cli_gem/cli'
 require_relative '../lib/ghibli_movies_cli_gem/api'
+require_relative '../lib/ghibli_movies_cli_gem/film'
 
 module GhibliMoviesCliGem
   # Your code goes here...
